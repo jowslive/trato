9683846d7d48fbedaea21fd18404fa18b94c78be
Joao Brufatto <joao.brufatto@gmail.com>
2021-02-22 14:46:28 -0300

    Adding validations to Models

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,7 @@
 class Job < ApplicationRecord
   has_many :users_jobs_applieds
   belongs_to :user
+
+  validates :cnpj, presence: true
+  validates :amount, presence: true, numericality: { only_integer: true }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates_format_of :name, with: /\A[^0-9`!@#$%\^&*+_=]+\z/
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 4 }
 end
