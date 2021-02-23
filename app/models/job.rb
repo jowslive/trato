ab2a81ca7f2c48751845143a751af13c65486a00
Joao Brufatto <joao.brufatto@gmail.com>
2021-02-23 17:08:57 -0300

    Added controller, views, actions & changed DB schema

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,6 +2,8 @@ class Job < ApplicationRecord
   has_many :enrollments
   belongs_to :user
 
-  validates :cnpj, presence: true
-  validates :amount, presence: true, numericality: { only_integer: true }
+  validates :name, presence: true
+  validates :content, presence: true
+  validates :cnpj, presence: true, length: { is: 14 }, numericality: { only_integer: true }
+  validates :amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
