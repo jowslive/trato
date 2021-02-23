fdbeb30ada1a4f8addbf6615c0d00b91f72f77f2
Joao Brufatto <joao.brufatto@gmail.com>
2021-02-23 13:01:18 -0300

    Fixing Routes and Adding Jobs Controller & Views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,8 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name encrypted_password])
   end
+
+  def after_sign_in_path_for(_resource)
+    jobs_path || root_path
+  end
 end
