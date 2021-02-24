f965f3526690b6ffd2ddd920aa10c9514c794812
Joao Brufatto <joao.brufatto@gmail.com>
2021-02-24 14:26:18 -0300

    Added account settings and customized navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :jobs
+  has_many :jobs, dependent: :destroy
   has_many :enrollments
+  has_one_attached :photo
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates_format_of :name, with: /\A[^0-9`!@#$%\^&*+_=]+\z/
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 4 }
+  # validates :email, presence: true, uniqueness: true
+  # validates :password, presence: true, length: { minimum: 4 }
 end
