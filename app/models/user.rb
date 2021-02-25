6b34c9aef52269774738082a0d29f684063931c3
Joao Brufatto <joao.brufatto@gmail.com>
2021-02-25 13:35:56 -0300

    Delete enrollments togheter with users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :jobs, dependent: :destroy
-  has_many :enrollments
+  has_many :enrollments, dependent: :destroy
   has_one_attached :photo
 
   devise :database_authenticatable, :registerable,
