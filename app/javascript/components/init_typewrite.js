1215ed1392db191e95034b459e26569d3431b533
Joao Brufatto <joao.brufatto@gmail.com>
2021-02-26 14:57:24 -0300

    Added tooltip

--- app/javascript/components/init_typewrite.js
+++ app/javascript/components/init_typewrite.js
@@ -1,11 +1,15 @@
 import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
-  new Typed('#banner-typed-text', {
-    strings: ["networks", "experiences", "internships", "a lot of coffee"],
-    typeSpeed: 150,
-    cursorChar: ''
-  });
+  const banner = document.getElementById("banner-typed-text");
+
+  if (banner) {
+    new Typed(banner, {
+      strings: ["networks", "experiences", "internships", "a lot of coffee"],
+      typeSpeed: 150,
+      cursorChar: ''
+    });
+  }
 }
 
 export { loadDynamicBannerText };
